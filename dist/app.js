9051021954d2f799c6afb5812ddcacd7af8f0bb0
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-12-27 15:04:39 +0530

    Decorators in the learning

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,55 @@
 "use strict";
-const names = ['max', 'anna'];
-console.log(names);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(logstring) {
+    console.log("Looger Factory");
+    return function (constructor) {
+        console.log(logstring);
+        console.log(constructor);
+    };
+}
+function withTemplate(template, hookId) {
+    return function (constructor) {
+        console.log("Rendering template");
+        const hookel = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookel) {
+            hookel.innerHTML = template;
+            hookel.querySelector('h1').textContent = p.name;
+        }
+    };
+}
+let Person = class Person {
+    constructor() {
+        this.name = 'Max';
+        console.log('Creating person object....');
+    }
+};
+Person = __decorate([
+    Logger('Logger...'),
+    withTemplate('<h1>My Person Object</h1>', 'app')
+], Person);
+const pers = new Person();
+console.log(pers);
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("give the valid number");
+        }
+    }
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,102 +1,161 @@
-// // // // // // // Code goes here!
-// // // // // // // console.log("abhishek kumar singh ")
+// // // // // // // // Code goes here!
+// // // // // // // // console.log("abhishek kumar singh ")
+// // // // // // // // const names:Array<string>=[];
+// // // // // // // // const names=["Abhishe","kumar","singh"];
+// // // // // // // // const names:any[]=[]
 // // // // // // // const names:Array<string>=[];
-// // // // // // // const names=["Abhishe","kumar","singh"];
-// // // // // // // const names:any[]=[]
-// // // // // // const names:Array<string>=[];
-// // // // // // // names[0].split('');
-// // // // // // const promise:Promise<string>=new Promise((resolve,reject)=>{
-// // // // // //     setTimeout(() => {
-// // // // // //         resolve('This is done')
+// // // // // // // // names[0].split('');
+// // // // // // // const promise:Promise<string>=new Promise((resolve,reject)=>{
+// // // // // // //     setTimeout(() => {
+// // // // // // //         resolve('This is done')
         
-// // // // // //     }, 2000);
-// // // // // // })
+// // // // // // //     }, 2000);
+// // // // // // // })
 
-// // // // // function merge<T,U>(objA:T,objB:U){
-// // // // //     return Object.assign(objA , objB)
-// // // // // }
-// // // // // const mergeObj=merge<{name:string,{age:number}>({name:"Max"},{age:30});
-// // // // // // mergeObj.name
+// // // // // // function merge<T,U>(objA:T,objB:U){
+// // // // // //     return Object.assign(objA , objB)
+// // // // // // }
+// // // // // // const mergeObj=merge<{name:string,{age:number}>({name:"Max"},{age:30});
+// // // // // // // mergeObj.name
 
-// // // // // function merge<T,U>(objA:T,objB:U):T&U{
-// // // // //     return Object.assign({},objA,objB);
+// // // // // // function merge<T,U>(objA:T,objB:U):T&U{
+// // // // // //     return Object.assign({},objA,objB);
+// // // // // // }
+// // // // // // const mergeObj=merge({name:"abhishek",hobbies:['sports']},{age:30});
+// // // // // // console.log(mergeObj)
+// // // // // interface Lengthy{
+// // // // //     length:number;
 // // // // // }
-// // // // // const mergeObj=merge({name:"abhishek",hobbies:['sports']},{age:30});
-// // // // // console.log(mergeObj)
-// // // // interface Lengthy{
-// // // //     length:number;
+// // // // // function countAndPrint<T extends Lengthy>(element:T):[T,string]{
+// // // // //     let descriptionTest='Got no value.';
+// // // // //     if(element.length===1){
+// // // // //         descriptionTest='Got1 element';
+// // // // //     }
+// // // // //     else if(element.length>1){
+// // // // //         descriptionTest='Got'+element.length+'elements';
+// // // // //     }
+// // // // //     return [element,descriptionTest];
+// // // // // }
+// // // // // console.log(countAndPrint(["sports","cooking"
+// // // // // ]))
+// // // // function extractAndConver(obj:object,key:string){
+// // // //     return obj[key];
 // // // // }
-// // // // function countAndPrint<T extends Lengthy>(element:T):[T,string]{
-// // // //     let descriptionTest='Got no value.';
-// // // //     if(element.length===1){
-// // // //         descriptionTest='Got1 element';
-// // // //     }
-// // // //     else if(element.length>1){
-// // // //         descriptionTest='Got'+element.length+'elements';
-// // // //     }
-// // // //     return [element,descriptionTest];
-// // // // }
-// // // // console.log(countAndPrint(["sports","cooking"
-// // // // ]))
-// // // function extractAndConver(obj:object,key:string){
-// // //     return obj[key];
+// // // // console.log(cons)
+
+
+
+
+// // // class DataStorage<T> {
+// // //     private data: T[] = []; // Explicitly define data as an array of type T
+
+// // //     addItem(item: T): void {
+// // //         this.data.push(item);
+// // //     }
+
+// // //     removeItem(item: T): void {
+// // //         const index = this.data.indexOf(item);
+// // //         if (index !== -1) {
+// // //             this.data.splice(index, 1);
+// // //         }
+// // //     }
+
+// // //     getItems(): T[] {
+// // //         return [...this.data]; // Return a copy of the array
+// // //     }
 // // // }
-// // // console.log(cons)
 
+// // // const textStorage = new DataStorage<string>();
+// // // textStorage.addItem("Max");
+// // // textStorage.addItem("Manu");
+// // // textStorage.removeItem("Max");
+// // // console.log(textStorage.getItems()); // Output: ["Manu"]
 
+// // // const numberStorage=new DataStorage<number>();
+// // // const objStorage=new DataStorage<object>();
+// // // objStorage.addItem({name:"Max"});
+// // // objStorage.addItem({name:"Manu"});
+// // // objStorage.removeItem({name:"Manu"}); 
+// // // console.log(objStorage.getItems());
 
+// // interface CourseGoal{
+// //     title:string;
+// //     description:string;
+// //     completeUntil:Date;
+// // }
+// // function createCourseGoal(
+// //     title:string,
+// //     description:string,
+// //     date:Date
+// // ):CourseGoal{
+// //     let courseGoal:Partial<CourseGoal>={};
+// //     courseGoal.title=title;
+// //     courseGoal.description=description;
+// //     courseGoal.completeUntil=date;
+// //     return courseGoal as CourseGoal;
 
-// // class DataStorage<T> {
-// //     private data: T[] = []; // Explicitly define data as an array of type T
-
-// //     addItem(item: T): void {
-// //         this.data.push(item);
-// //     }
-
-// //     removeItem(item: T): void {
-// //         const index = this.data.indexOf(item);
-// //         if (index !== -1) {
-// //             this.data.splice(index, 1);
-// //         }
-// //     }
-
-// //     getItems(): T[] {
-// //         return [...this.data]; // Return a copy of the array
-// //     }
 // // }
 
-// // const textStorage = new DataStorage<string>();
-// // textStorage.addItem("Max");
-// // textStorage.addItem("Manu");
-// // textStorage.removeItem("Max");
-// // console.log(textStorage.getItems()); // Output: ["Manu"]
-
-// // const numberStorage=new DataStorage<number>();
-// // const objStorage=new DataStorage<object>();
-// // objStorage.addItem({name:"Max"});
-// // objStorage.addItem({name:"Manu"});
-// // objStorage.removeItem({name:"Manu"}); 
-// // console.log(objStorage.getItems());
-
-// interface CourseGoal{
-//     title:string;
-//     description:string;
-//     completeUntil:Date;
+// const names:Readonly<string[]>=['max','anna'];
+// // names.push('Manu');
+// // name.pop()
+// console.log(names)
+// function Logger(constructor:Function){
+//     console.log("Logging......")
+//     console.log(constructor);
 // }
-// function createCourseGoal(
-//     title:string,
-//     description:string,
-//     date:Date
-// ):CourseGoal{
-//     let courseGoal:Partial<CourseGoal>={};
-//     courseGoal.title=title;
-//     courseGoal.description=description;
-//     courseGoal.completeUntil=date;
-//     return courseGoal as CourseGoal;
+function Logger(logstring:string){
+    console.log("Looger Factory")
+    return function (constructor:Function){
+        console.log(logstring);
+        console.log(constructor)
 
-// }
+    }
+}
+function withTemplate(template:string,hookId:string){
+     return function(constructor:any){
+        console.log("Rendering template")
+        const hookel=document.getElementById(hookId);
+        const p = new constructor();
+        if(hookel){
+            hookel.innerHTML=template;
+            hookel.querySelector('h1')!.textContent=p.name;
+        }
+     }
+}
+@Logger('Logger...')
+@withTemplate('<h1>My Person Object</h1>','app')
+class Person{
+    name='Max';
+    constructor(){
+        console.log('Creating person object....');
+    }
+}
+const pers=new Person();
+console.log(pers);
 
-const names:Readonly<string[]>=['max','anna'];
-// names.push('Manu');
-// name.pop()
-console.log(names)
+function Log(target:any,propertName:string|Symbol){
+    console.log('Property decorator')
+
+}
+
+class Product{
+    @Log
+    title:string;
+    private _price:number;
+    set price(val:number){
+        if(val>0){
+            this._price=val;
+        }else{
+            throw new Error("give the valid number");
+        }
+    }
+    constructor(t:string,p:number){
+        this.title=t;
+        this._price=p;
+    }
+
+    getPriceWithTax(tax:number){
+        return this._price*(1+tax)
+    }
+}
