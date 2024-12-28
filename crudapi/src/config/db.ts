08d5d2badc6c85d6f01b4f5b19d48a48a62dbca6
Abhishek kumar singh <abhisheksinghbhtr967@gmail.com>
2024-12-28 10:25:21 +0530

    pushing code with security of database

--- crudapi/src/config/db.ts
+++ crudapi/src/config/db.ts
@@ -1,8 +1,5 @@
-// import mongoose from "mongoose";
-// const connectDB=async()=>{
-// await mongoose.connect("mongodb+srv://pschauhan468:Hippro%40ripple@cluster0.h7p2m.mongodb.net/");
-// }
+
 import mongoose, { mongo } from "mongoose";
 
-const database=mongoose.connect("mongodb+srv://pschauhan468:Hippro%40ripple@cluster0.h7p2m.mongodb.net/");
+const database=mongoose.connect("");
 export default database;
